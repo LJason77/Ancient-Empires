0a6ed1590f7c393be4fa0c7fbaf0fc79bf660067
LJason <ljason@ljason.cn>
2021-09-23 17:31:30 +0800

    init
    Signed-off-by: LJason <ljason@ljason.cn>

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 更新日志
+
+## [0.0.0] - 2021-09-23
+
+- 创建项目
+
+[0.0.0]: https://github.com/LJason77/Ancient-Empires/releases/tag/v0.0.0
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![deny(clippy::pedantic)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
